4c162d885e11951ea8b31b44276656b23533b453
mzecic <matejzecic@gmail.com>
2022-09-30 18:47:37 -0400

    adds index, new, and post functionality

---
+++ controllers/flights.js
@@ -0,0 +1,33 @@
+const Flight = require("../models/flight");
+module.exports = {
+    index,
+    new: newFlight,
+    create,
+}
+
+
+function index(req, res) {
+    Flight.find({}, function(err, flights) {
+        res.render("flights/index", {
+            flights
+        });
+    })
+
+}
+
+
+function newFlight(req, res) {
+    res.render("flights/new");
+}
+
+function create(req, res) {
+    console.log(req.body)
+    const flight = new Flight(req.body)
+    flight.save(function(err) {
+        if(err) {
+            console.log(err);
+            res.redirect("/flights/new");
+        }
+        res.redirect("/flights")
+    })
+}
---
+++ models/flight.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ["American", "Southwest", "United"]
+    },
+    airport: {
+        type: String,
+        enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+        default: "DEN"
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            return new Date(parseInt("created_at".slice(0, 4) + 1))
+        }
+    }
+}, {
+    timestamps: {
+        createdAt: "created_at"
+    }
+});
+
+
+module.exports = mongoose.model("Flight", flightSchema);
